340f3bc74ff91a78d14847188043dba25ff9ad06
Konrad (PC) <kolego199@gmail.com>
2021-12-09 17:08:21 +0100

    v6.4 - allow user to use weekday abbreviations for !plan command

--- modules/main.py
+++ modules/main.py
@@ -679,27 +679,31 @@ def get_lesson_plan(message: discord.Message) -> tuple[bool, str or discord.Embe
         current_day = -1
         try:
             # This 'try' clause raises RuntimeError if the input is invalid for whatever reason
-            try:
-                # Check if the input is a number
-                if not Weekday.monday <= int(args[1]) <= Weekday.friday:
-                    # It is, but of invalid format
-                    raise RuntimeError(f"{args[1]} is not a number between 1 and 5.")
-                else:
-                    # It is, and of correct format
-                    current_day = int(args[1]) - 1
-            except ValueError:
-                # The input is not a number.
-                # Check if it is a day of the week
-                for i, weekday in enumerate(weekday_names):
-                    if weekday.startswith(args[1]):
-                        # The input is a valid weekday name.
-                        current_day = i
-                        break
-                # 'current_day' will have the default value of -1 if the above for loop didn't find any matches
-                if current_day == -1:
-                    # The input is not a valid weekday name.
-                    # ValueError can't be used since it has already been caught
-                    raise RuntimeError(f"invalid weekday name: {args[1]}")
+            weekday_abbreviations = {"pn": 0, "śr": 2, "sr": 3, "pt": 4}
+            if args[1] in weekday_abbreviations:
+                current_day = weekday_abbreviations[args[1]]
+            else:
+                try:
+                    # Check if the input is a number
+                    if not 1 <= int(args[1]) <= 5:
+                        # It is, but of invalid format
+                        raise RuntimeError(f"{args[1]} is not a number between 1 and 5.")
+                    else:
+                        # It is, and of correct format
+                        current_day = int(args[1]) - 1
+                except ValueError:
+                    # The input is not a number.
+                    # Check if it is a day of the week
+                    for i, weekday in enumerate(weekday_names):
+                        if weekday.startswith(args[1]):
+                            # The input is a valid weekday name.
+                            current_day = i
+                            break
+                    # 'current_day' will have the default value of -1 if the above for loop didn't find any matches
+                    if current_day == -1:
+                        # The input is not a valid weekday name.
+                        # ValueError can't be used since it has already been caught
+                        raise RuntimeError(f"invalid weekday name: {args[1]}")
         except RuntimeError as e:
             attempt_debug_message(f"Handling exception with args: '{' '.join(args[1:])}' ({type(e).__name__}: \"{e}\")")
             return False, f"{Emoji.warning} Należy napisać po komendzie `{prefix}plan` numer dnia (1-5) " \
