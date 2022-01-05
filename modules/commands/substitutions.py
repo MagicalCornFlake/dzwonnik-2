839832475b948ca922349ff74b54fdb29625ef65
Konrad (PC) <kolego199@gmail.com>
2022-01-05 01:22:21 +0100

    complete substitutions embed

--- modules/commands/substitutions.py
+++ modules/commands/substitutions.py
@@ -21,10 +21,42 @@ def get_substitutions_embed(_: Message = None) -> tuple[bool, Embed or str]:
         data = substitutions_crawler.get_substitutions()[0]
     except Exception as e:
         ex: str = util.format_exception_info(e)
-        bot.send_log(f"Error! Received an invalid response from the web request. Exception trace:\n{ex}", force=True)
+        error_message = f"Error! Received an invalid response from the web request. Exception trace:\n{ex}"
+        bot.send_log(error_message, force=True)
         return False, web.get_error_message(e)
-    msg = f"Zastępstwa na {datetime.now():%d.%m.%Y}:"
-    embed = Embed(title="Zastępstwa", description=msg)
-    embed.add_field(name="Dane", value=data, inline=False)
-    embed.set_footer(text=f"Użyj komendy {bot.prefix}zast, aby pokazać tą wiadomość.")
+
+    # Initialise the embed
+    date = datetime.strptime(data["date"], "%Y-%m-%d")
+    description = f"Zastępstwa na {date:%d.%m.%Y}:"
+    embed = Embed(title="Zastępstwa", description=description)
+    footer = f"Użyj komendy {bot.prefix}zast, aby pokazać tą wiadomość."
+    embed.set_footer(text=footer)
+
+    # Add fields
+    teachers = ', '.join(data["teachers"])
+    embed.add_field(name="Nauczyciele", value=teachers, inline=False)
+    embed.add_field(name="Informacje ogólne", value=data["misc"], inline=False)
+
+    for period in data["lessons"]:
+        class_msgs = []
+        for class_name, substitutions in data["lessons"][period].items():
+            sub_msgs = []
+            for sub_info in substitutions:
+                groups = sub_info.get("groups")
+                group_text = f"(gr. {', '.join(groups)}) — " if groups else ""
+                sub_msgs.append(f"{group_text}*{sub_info['details']}*")
+            class_msgs.append(f"**{class_name}**: {' | '.join(sub_msgs)}")
+        time = util.get_formatted_period_time(period)
+        field_args = {
+            "name": f"Lekcja {period} ({time})",
+            "value": '\n'.join(class_msgs)
+        }
+        embed.add_field(**field_args, inline=False)
+    for table in data["tables"]:
+        field_args = {
+            "name": table["heading"],
+            "value": f"{table['rows']} lekcje",
+        }
+        embed.add_field(**field_args, inline=False)
+    embed.add_field(name="Lekcje odwołane", value=data["cancelled"])
     return True, embed
