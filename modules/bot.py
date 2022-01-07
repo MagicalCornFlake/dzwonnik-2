dec02c4a185206b9b1c095bb6671b1ecd2e8b490
Konrad (PC) <kolego199@gmail.com>
2022-01-07 02:35:49 +0100

    remove possibly redundant sleep

--- modules/bot.py
+++ modules/bot.py
@@ -531,7 +531,6 @@ async def check_for_substitutions_updates() -> None:
 async def close() -> None:
     """Sets the bot's Discord status to 'offline' and terminates it."""
     await client.change_presence(status=discord.Status.offline)
-    await asyncio.sleep(0.5)
     await client.close()
 
 
