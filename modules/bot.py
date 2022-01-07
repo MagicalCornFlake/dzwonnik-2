e3c8ea01fcb15834a7b76e7f239a5bbb7fc5381e
Konrad (PC) <kolego199@gmail.com>
2022-01-07 02:55:54 +0100

    increase sleep in close to 500 ms

--- modules/bot.py
+++ modules/bot.py
@@ -531,8 +531,8 @@ async def check_for_substitutions_updates() -> None:
 async def close() -> None:
     """Sets the bot's Discord status to 'offline' and terminates it."""
     await client.change_presence(status=discord.Status.offline)
-    # Sleep for 250 ms to ensure that the client.close() coroutine is the last to execute.
-    await asyncio.sleep(0.25)
+    # Sleep for 500 ms to ensure that the client.close() coroutine is the last to execute.
+    await asyncio.sleep(0.5)
     await client.close()
 
 
