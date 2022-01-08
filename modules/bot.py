8562930e8f0e9d74bdef032c87130b535597eff6
Konrad (PC) <kolego199@gmail.com>
2022-01-08 17:57:16 +0100

    fix last commit; 'content' kwarg placed correctly

--- modules/bot.py
+++ modules/bot.py
@@ -455,9 +455,9 @@ async def wait_before_starting_loop() -> None:
         last_msg_chnl: discord.TextChannel = await client.fetch_channel(msg_info["channel_id"])
         last_msg: discord.Message = await last_msg_chnl.fetch_message(msg_info["message_id"])
         if msg_info["is_restart"]:
-            await last_msg.edit(RESTARTED_BOT_MSG)
+            await last_msg.edit(content=RESTARTED_BOT_MSG)
         else:
-            await last_msg.reply(content=RESTARTED_BOT_MSG)
+            await last_msg.reply(RESTARTED_BOT_MSG)
     finally:
         # Reset the on exit message so that it is not replied to twice.
         file_manager.on_exit_msg = {}
