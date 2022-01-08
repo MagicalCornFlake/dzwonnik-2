603c9170266d0d0bb834379528828d926409fd5e
Konrad (PC) <kolego199@gmail.com>
2022-01-08 22:14:57 +0100

    update methods of accessing lucky numbers cache

--- modules/util/api/lucky_numbers.py
+++ modules/util/api/lucky_numbers.py
@@ -42,11 +42,10 @@ def update_cache() -> dict[str, str or list[int or str]]:
 
     Returns the old cache so that it can be compared with the new one.
     """
-    # TODO: Change cached_data storage method
-    global cached_data
     old_cache = dict(cached_data or {})
+    cached_data.clear()
     res = web.make_request(SOURCE_URL, ignore_request_limit=True)
-    cached_data = res.json()
+    cached_data.update(res.json())
     # If the date string is present in the dictionary, convert it into a date object.
     if cached_data["date"]:
         data_timestamp = datetime.strptime(cached_data["date"], "%d/%m/%Y")
@@ -55,7 +54,15 @@ def update_cache() -> dict[str, str or list[int or str]]:
 
 
 def serialise(data: dict = None, pretty: bool = False) -> dict or str:
-    """Returns the cached data as a JSON-serialisable dictionary."""
+    """Returns the cached data as a JSON-serialisable dictionary.
+
+    Arguments:
+        data -- a dictionary to serialise. If not specified, the lucky numbers cached data is used.
+        pretty -- if True, the returned value is a string representing the pretty-printed data.
+
+    A dictionary with all values stringified using the str function, or the pretty-printed form of
+    that dictionary.
+    """
     temp: dict = dict(data or cached_data or {})
     for key, value in temp.items():
         try:
