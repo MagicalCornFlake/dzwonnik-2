c60e4e1829ca6eb56222735c6a88021854b5d241
Konrad (PC) <kolego199@gmail.com>
2022-01-18 13:42:01 +0100

    optimise termination process

--- modules/commands/terminate.py
+++ modules/commands/terminate.py
@@ -10,26 +10,28 @@ from .. import bot, file_manager
 DESC = None
 
 
+RESTARTING_BOT_MSG = "Restarting bot..."
+EXITING_BOT_MSG = "Exiting program."
+
+
 def restart_bot(message: discord.Message) -> tuple[bool, str]:
     """Event handler for the 'restart' command."""
     ensure_sender_is_admin(message)
-    return "Restarting bot..."
+    return RESTARTING_BOT_MSG
 
 
 def exit_bot(message: discord.Message) -> tuple[bool, str]:
     """Event handler for the 'exit' command."""
     ensure_sender_is_admin(message)
     bot.restart_on_exit = False
-    return "Exiting program."
+    return EXITING_BOT_MSG
 
 
 async def terminate_bot(original_msg: discord.Message, reply_msg: discord.Message) -> None:
     """Save's the ID of the bot's exit message and terminates the bot client process."""
-    command_content: str = original_msg.content
-    is_restart = command_content.startswith(f"{bot.prefix}restart")
     file_manager.on_exit_msg = {
-        "is_restart": is_restart,
-        "channel_id": reply_msg.channel.id,
+        "is_restart": reply_msg.content == RESTARTING_BOT_MSG,
+        "channel_id": original_msg.channel.id,
         "message_id": reply_msg.id
     }
     bot.main_update_loop.stop()
